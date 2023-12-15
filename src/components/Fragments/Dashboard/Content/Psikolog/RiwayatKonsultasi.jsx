e76b169b3eddd7bd17b3778d444c0f1b028cbe8c
andrinugroho0 <nugrohoandri600@gmail.nom>
2023-12-16 01:51:32 +0700

    PUSH | Psikolog, Jadwal, Riwayat, Profile, Bantuan

--- src/components/Fragments/Dashboard/Content/Psikolog/RiwayatKonsultasi.jsx
+++ src/components/Fragments/Dashboard/Content/Psikolog/RiwayatKonsultasi.jsx
@@ -1,18 +1,40 @@
-function RiwayatKonsultasiPsikolog() {
+import React from 'react';
+  const RiwayatKonsultasiPsikolog = () => {
 
+    const data = React.useMemo(
+      () => [
+        { id: 1, tanggal: '2023-01-01', dokter: 'Dr. A', diagnosis: 'Flu' },
+        { id: 2, tanggal: '2023-02-15', dokter: 'Dr. B', diagnosis: 'Demam' },
+        { id: 3, tanggal: '2023-03-22', dokter: 'Dr. C', diagnosis: 'Sakit Kepala' },
+      ],
+      []
+    );
+  
     return (
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-  
-        <div className="max-w-xl mb-5 md:mx-auto sm:text-center lg:max-w-2xl">
-          <div className="mb-4">
-            <h1>Riwayat Konsultasi Psikolog</h1>
-          </div>
-  
-        </div>
-  
+      <div className="container mx-auto mt-8">
+        <table className="min-w-full bg-white-950 border border-gray-600">
+          <thead>
+            <tr className="bg-gray-100">
+              <th className="py-2 px-4 border-b">ID</th>
+              <th className="py-2 px-4 border-b">Tanggal</th>
+              <th className="py-2 px-4 border-b">Dokter</th>
+              <th className="py-2 px-4 border-b">Diagnosis</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row) => (
+              <tr key={row.id}>
+                <td className="py-2 px-4 border-b">{row.id}</td>
+                <td className="py-2 px-4 border-b">{row.tanggal}</td>
+                <td className="py-2 px-4 border-b">{row.dokter}</td>
+                <td className="py-2 px-4 border-b">{row.diagnosis}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-    )
+    );
+  };
   
-  }
-  
+
   export default RiwayatKonsultasiPsikolog;

9b8c09ab9af896ba0fb4e47af1f2a2b1283e6cf4
RetnoGumilang <retnogumilang76@gmail.com>
2023-12-24 14:00:25 +0700

    menambah fitur profil admin

--- src/components/Fragments/Dashboard/Content/Admin/index.jsx
+++ src/components/Fragments/Dashboard/Content/Admin/index.jsx
@@ -1,45 +1,46 @@
 // Content.js
-import React from 'react';
-import ManajemenPsikolog from './ManajemenPsikolog';
-import ManajemenPengguna from './ManajemenPengguna';
-import Laporan from './Laporan';
-import Bantuan from './Bantuan';
-import BerandaAdmin from './Beranda';
+import React from "react";
+import ManajemenPsikolog from "./ManajemenPsikolog";
+import ManajemenPengguna from "./ManajemenPengguna";
+import Bantuan from "./Bantuan";
+import BerandaAdmin from "./Beranda";
+import ProfilAdmin from "./Profil";
 
 const Content = ({ selectedMenu }) => {
   let renderedContent = null;
 
   switch (selectedMenu) {
-    case 'Beranda':
+    case "Beranda":
       renderedContent = <BerandaAdmin />;
       break;
-    case 'Manajemen Psikolog':
+    case "Manajemen Psikolog":
       renderedContent = <ManajemenPsikolog />;
       break;
-    case 'Manajemen Pasien':
+    case "Manajemen Pasien":
       renderedContent = <ManajemenPengguna />;
       break;
-    case 'Laporan':
-      renderedContent = <Laporan />;
+    case "Profil":
+      renderedContent = <ProfilAdmin />;
       break;
-    case 'Bantuan':
+    case "Bantuan":
       renderedContent = <Bantuan />;
       break;
-    
+
     default:
-      renderedContent = 
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-  
-        <div className="max-w-xl mb-5 md:mx-auto sm:text-center lg:max-w-2xl">
-          <div className="mb-4">
-          
-            <h1 className='text-3xl font-bold text-white bg-red-500 text-center'>Selamat Datang</h1>
-            <h1 className='text-3xl font-bold text-red-500 text-center'>Admin</h1>
+      renderedContent = (
+        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+          <div className="max-w-xl mb-5 md:mx-auto sm:text-center lg:max-w-2xl">
+            <div className="mb-4">
+              <h1 className="text-3xl font-bold text-white bg-red-500 text-center">
+                Selamat Datang
+              </h1>
+              <h1 className="text-3xl font-bold text-red-500 text-center">
+                Admin
+              </h1>
+            </div>
           </div>
-  
         </div>
-  
-      </div>
+      );
       break;
   }
 
